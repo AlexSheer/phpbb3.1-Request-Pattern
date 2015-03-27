1b007497c4732bdfd3fed894d39dab2fb3724f2c
AlexSheer <alex_sheer@inbox.lv>
2015-03-27 19:16:33 +0200

    Create ptrequest.php

---
+++ language/ru/ptrequest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* @package phpBB Extension - Request Pattern
+* @copyright (c) 2015 Sheer
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'PATTERN'	=> 'Шаблон запроса',
+));
