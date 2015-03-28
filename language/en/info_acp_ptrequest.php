5b500fa3ba5802ee389cec30e71060fed65b8296
rxu <rxu@mail.ru>
2015-03-28 19:52:06 +0700

    Add English language. Fix 404 when URL rewriting is disabled.

---
+++ language/en/info_acp_ptrequest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+*
+* info_acp_ptrequest [Russian]
+*
+* @package Request Pattern
+* @copyright (c) 2015 Sheer
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+$lang = array_merge($lang, array(
+	'ACP_REQUEST_PATTERN'			=> 'Request template',
+	'ACP_REQUEST_PATTERN_MANAGE'	=> 'Manage request template',
+	'ACP_REQUEST_PATTERN_EXPLAIN'	=> 'Here you can set, edit, reorder and delete questions for request template.',
+
+	'QUESTION_NAME'				=> 'Question',
+	'EXPLAIN'					=> 'Explanation',
+	'DELETE_MARKED_SUCESS'		=> 'Selected questions was successfully deleted',
+	'DELETE_SUCESS'				=> 'All questions was successfully deleted',
+	'UPDATE_SUCCESS'			=> 'Selected questions was successfully changed',
+	'UPDATE_FAIL'				=> 'No items have been selected',
+	'EMPTY_QUESTION'			=> 'You did not enter a question',
+	'ADD_SUCCESS'				=> 'Question was added successfully',
+	'CHANGE_SELECTED'			=> 'Change selected questions',
+));
