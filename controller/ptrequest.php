88d6f6743392ad81480926179d2fd8147ec73687
AlexSheer <alex_sheer@inbox.lv>
2015-03-28 13:33:19 +0200

    Update ptrequest.php

--- controller/ptrequest.php
+++ controller/ptrequest.php
@@ -21,38 +21,51 @@ class ptrequest
 		\phpbb\db\driver\driver_interface $db,
 		\phpbb\template\template $template,
 		\phpbb\user $user,
+		$cache,
 		$request_table
 	)
 	{
 		$this->db = $db;
 		$this->template = $template;
 		$this->user = $user;
+		$this->cache = $cache;
 		$this->request_table = $request_table;
 	}
 
 	public function main()
 	{
-		$question_count = 0;
+		if (($data = $this->cache->get('_pattern_request')) === false)
+		{
+			$data = array();
 
-		$sql = 'SELECT *
-			FROM ' . $this->request_table . '
-			ORDER BY order_question';
-		$result = $this->db->sql_query($sql);
-		while($row = $this->db->sql_fetchrow($result))
+			$sql = 'SELECT *
+				FROM ' . $this->request_table . '
+				ORDER BY order_question';
+			$result = $this->db->sql_query($sql);
+			while($row = $this->db->sql_fetchrow($result))
+			{
+				$id = $row['order_question'] - 1;
+				$data[$id]['key'] = $row['order_question'];
+				$data[$id]['value'] = $row['question'];
+				$data[$id]['explain'] = $row['question_explain'];
+			}
+			$this->db->sql_freeresult($result);
+			$this->cache->put('_pattern_request', $data);
+		}
+
+		foreach($data as $key => $value)
 		{
 			$this->template->assign_block_vars('patternrow', array(
-				'L_KEY'		=> 'q_' . $row['order_question'] . '',
-				'L_VALUE'	=> $row['question'],
-				'L_EXPLAIN'	=> $row['question_explain'],
-				'ID'		=> $row['order_question'] - 1,
+				'L_KEY'		=> 'q_' . $data[$key]['key'] . '',
+				'L_VALUE'	=> $data[$key]['value'],
+				'L_EXPLAIN'	=> $data[$key]['explain'],
+				'ID'		=> $key,
 				)
 			);
-			$question_count++;
 		}
-		$this->db->sql_freeresult($result);
 
 		$this->template->assign_vars(array(
-			'OPTIONS_NUMBER'		=> $question_count,
+			'OPTIONS_NUMBER'		=> sizeof($data),
 			)
 		);
 
